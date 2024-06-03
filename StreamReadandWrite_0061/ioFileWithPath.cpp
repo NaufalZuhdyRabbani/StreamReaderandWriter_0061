1fc3328f5cd50cd1678249b89610693a22d42d4a
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2024-06-03 14:43:09 +0700

    membuka dalam mode menulis

---
+++ StreamReadandWrite_0061/ioFileWithPath.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Memasukan Nama File : ";
+	cin >> NamaFile;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	// menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter data satu baris
+	}
+}
